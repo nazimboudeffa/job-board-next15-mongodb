1cfabad8593d594cf8a767728a9bef171d84b52a
Nazim Boudeffa <boudeffa@gmail.com>
2025-06-02 21:43:26 +0200

    improved profiles

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function PP() {
+    return (
+        <main className="m-auto my-10 max-w-5xl space-y-5 px-3 text-center">
+        <h1 className="font-bold">About</h1>
+        <p>neoFreelance is a place to connect Talents with Opportunities.</p>
+      </main>
+    )
+}
