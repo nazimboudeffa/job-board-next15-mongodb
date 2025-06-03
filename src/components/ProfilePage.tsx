058db02f572f43a8173fd989342c52bd3a509964
Nazim Boudeffa <boudeffa@gmail.com>
2025-06-03 17:47:31 +0200

    reorganised the code

--- src/components/ProfilePage.tsx
+++ src/components/ProfilePage.tsx
@@ -5,16 +5,7 @@ import { useEffect, useState } from "react";
 import Profile from "@/lib/types/profile";
 import Image from "next/image";
 import Markdown from "react-markdown";
-
-import avatar1 from "@/assets/avatar-1.png";
-import avatar2 from "@/assets/avatar-2.png";
-import avatar3 from "@/assets/avatar-3.png";
-
-const randomImages = [
-  { name: avatar1, source: "avatar-1.png" },
-  { name: avatar2, source: "avatar-2.png" },
-  { name: avatar3, source: "avatar-3.png" },
-];
+import { getAvatarSource } from "@/lib/avatars";
 
 interface JobPageProps {
   readonly username: string;
@@ -45,13 +36,6 @@ export default function JobPage({
         setLoading(false);
       });
   }, [username]);
-
-  const getAvatarSource = (avatar: string | null) => {
-    console.log('Avatar:', avatar);
-    const avatarObj = randomImages.find((img) => img.source === avatar);
-    console.log('Avatar:', avatarObj);
-    return avatarObj ? avatarObj.name.src : avatar1.src;
-  };
 
   if (loading) return <div className="p-4 text-center">Loading profile...</div>;
   if (error) return <div className="p-4 text-center text-red-600">{error}</div>;
